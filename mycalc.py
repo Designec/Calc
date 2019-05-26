601d96c0d23c3eadab1ad7dfdd8be2c196b84430
a10neca <c-alien@yandex.ru>
2019-05-26 17:50:19 +0500

    second

--- mycalc.py
+++ mycalc.py
@@ -6,23 +6,6 @@ window = Tk()
 window.title('Calculator')
 
 
-def clear():
-    entry.delete(0, END)
-
-
-def eq(eqkey):
-    if eqkey == '=':
-        allsimbols = '-+01234567890.*/='
-        if entry.get()[0] not in allsimbols:
-            messagebox.showerror('Ошибка!', 'Вы ввели не число!')
-        try:
-            result = eval(entry.get())
-            entry.delete(0, END)
-            entry.insert(END, result)
-        except:
-            messagebox.showerror("Ошибка!", 'Проверь правильность данных')
-
-
 def calc(key):
     if key == '+/-':
         if '=' in entry.get():
@@ -34,7 +17,18 @@ def calc(key):
                 entry.insert(0, '-')
         except IndexError:
             pass
-
+    elif key == '=':
+        allsimbols = '-+01234567890.*/='
+        if entry.get()[0] not in allsimbols:
+            messagebox.showerror('Ошибка!', 'Вы ввели не число!')
+        try:
+            result = eval(entry.get())
+            entry.delete(0, END)
+            entry.insert(END, result)
+        except:
+            messagebox.showerror("Ошибка!", 'Проверь правильность данных')
+    elif key == 'C':
+        entry.delete(0, END)
     else:
         if '=' in entry.get():
             entry.delete(0, END)
@@ -58,10 +52,10 @@ c = 0
 
 for i in btnList:
     if i == '=':
-        ttk.Button(window, text=i, command=lambda x=i: eq(x)).grid(column=0, columnspan=4, row=8, ipadx='10',
+        ttk.Button(window, text=i, command=lambda x=i: calc(x)).grid(column=0, columnspan=4, row=8, ipadx='10',
                                                                    ipady='10', sticky=W + E)
     elif i == 'C':
-        ttk.Button(window, text=i, command=clear).grid(column=0, columnspan=4, row=1, ipadx='10',
+        ttk.Button(window, text=i, command=lambda x=i: calc(x)).grid(column=0, columnspan=4, row=1, ipadx='10',
                                                        ipady='10', sticky=W + E)
     else:
         ttk.Button(window, text=i, command=lambda x=i: calc(x)).grid(row=r, column=c, ipadx='10', ipady='10')
